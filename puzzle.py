9e2b92240ed9db3f790dd496085e81b42ff4e662
Karla Hernandez <karlahrnndz.dev@gmail.com>
2023-09-09 16:52:17 -0700

    Created action matrix for non-square puzzles.

--- puzzle.py
+++ puzzle.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.typing import ArrayLike
 from numpy.random import default_rng
-from scipy.sparse import dok_array, dia_array
+from scipy.sparse import dok_array, dia_array, csc_array, csr_array
 from typing import Union
 import warnings
 
@@ -23,54 +23,65 @@ class Puzzle:
 
     def __init__(self,
                  state: ArrayLike = None,
-                 dim: int = None,
+                 dim: Union[int, tuple] = None,
                  seed: int = None):
 
         # Initial value check and config
         self.state, self.dim = self.value_check(state, dim)  # TODO - Add solution between any states
         self.gen = default_rng(seed=seed)
-        self.no_switches = self.dim ** 2
+        self.no_switches = self.dim[0] * self.dim[1]
         self.action_mtx = None
         self.solution = None
         self.solved = False
+        self.max_dim = max(self.dim[0], self.dim[1])
 
         # Generate state if required
         if not self.state:
-            self.state = self.generate_state(dim)
+            self.state = self.generate_state()
 
     @staticmethod
     def value_check(state, dim):
         if state:
-            state = dok_array(state, dtype=np.int8)
 
-            # Make sure state has the correct dimensions
-            if state.shape[0] != state.shape[1]:
-                raise ValueError("`state` should be a 2D Python array_like object "
-                                 "with the same number or rows as columns.")
+            if state.shape[0] > state.shape[1]:
+                warnings.warn(f"`state.shape[0] > state.shape[1]` while this code assumes "
+                              f"`state.shape[0] < state.shape[1]`. Will transpose the problem.")
+                state = np.array(state).T
+
+            state = dok_array(state, dtype=np.int8)
 
             # Warn when dim is present as well as staten and force dim to match state
             if dim:
                 warnings.warn(f"Both `state` and `dim` were provided."
-                              f"Setting `dim = state.shape[0]`.")
+                              f"Setting `dim = (state.shape[0], state.shape[1])`.")
 
-            dim = state.shape[0]
+            dim = (state.shape[0], state.shape[1])
 
         elif not dim:
             raise ValueError("One of `state` and `dim` must be provided.")
 
+        elif isinstance(dim, int):
+            dim = (dim, dim)
+
+        elif dim[0] > dim[1]:
+            warnings.warn(f"`dim[0] > dim[1]` while this code assumes `dim[0] <= dim[1]`. "
+                          f"Will transpose the problem so that `dim[0] < dim[1]`.")
+            dim = (dim[1], dim[0])
+
         return state, dim
 
-    def generate_state(self, dim):
-        state = dok_array((dim, dim), dtype=np.int8)
-        for i in range(dim):
-            for j in range(dim):
+    def generate_state(self):
+
+        state = dok_array(self.dim, dtype=np.int8)
+        for i in range(self.dim[0]):
+            for j in range(self.dim[1]):
 
                 press_switch = self.gen.binomial(1, 0.5, 1)[0]
                 if press_switch:
                     affected = {(i, j),
-                                (i, min(j + 1, dim - 1)),
+                                (i, min(j + 1, self.dim[1] - 1)),
                                 (i, max(j - 1, 0)),
-                                (min(i + 1, dim - 1), j),
+                                (min(i + 1, self.dim[0] - 1), j),
                                 (max(i - 1, 0), j)}
 
                     for button in affected:
@@ -78,27 +89,29 @@ class Puzzle:
 
         return state
 
-    # @staticmethod
-    # def prettify(matrix):
-    #     return matrix.toarray().astype(int)
-
     def create_action_mtx(self):
 
-        if self.dim == 1:
-            return dok_array([[1]], shape=(1, 1), dtype=np.int8)
+        if self.dim == (1, 1):  # TODO - check this
+            return dok_array([[1]], shape=self.dim, dtype=np.int8)
 
-        else:
-            # Construct the LHS of the extended matrix (the unravelled action matrix)
-            data = np.ones((3, self.no_switches))
+        else:  # Construct the LHS of the extended matrix (the unravelled action matrix)
+            data = np.ones((3, self.max_dim ** 2))
             data = np.append(data,
-                             [np.tile([1] * (self.dim - 1) + [0], self.no_switches // 3 + 1)[:self.no_switches],
-                              np.tile([0] + [1] * (self.dim - 1), self.no_switches // 3 + 1)[:self.no_switches]],
+                             [np.tile([1] * (self.max_dim - 1) + [0], (self.max_dim ** 2) // 3 + 1)[:self.max_dim ** 2],
+                              np.tile([0] + [1] * (self.max_dim - 1), (self.max_dim ** 2) // 3 + 1)[:self.max_dim ** 2]],
                              axis=0)
-            offsets = np.array([0, -self.dim, self.dim, -1, 1])
+            offsets = np.array([0, -self.max_dim, self.max_dim, -1, 1])
+            action_mtx = dia_array((data, offsets),
+                                   shape=(self.max_dim ** 2, self.max_dim ** 2),
+                                   dtype=np.int8)
 
-        return dok_array(dia_array((data, offsets),
-                                   shape=(self.no_switches, self.no_switches),
-                                   dtype=np.int8))
+        if self.no_switches != self.max_dim ** 2:  # Need to correct action matrix (always more columns than rows)
+            action_mtx = csc_array(action_mtx)[[ele for ele in range(self.no_switches)], :]
+            action_mtx = csr_array(action_mtx)[:, [ele for ele in range(self.no_switches)]]
+
+        action_mtx = dok_array(action_mtx)
+
+        return action_mtx
 
     def unravel_state(self, state: Union[ArrayLike, int]):
 
@@ -187,6 +200,7 @@ class Puzzle:
 # =================================================================== #
 
 if __name__ == "__main__":
-    my_puzzle = Puzzle(dim=10, seed=SEED)
+    my_puzzle = Puzzle(dim=(3, 2), seed=SEED)
+    print(my_puzzle.state.toarray().astype(int))
     my_puzzle.solve(desired_state=1)
-    print(my_puzzle.solution)
+    print('hi')
