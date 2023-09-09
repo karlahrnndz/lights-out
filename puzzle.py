4d8e880dbf4b4655311cf42240535b7d21099e70
Karla Hernandez <karlahrnndz.dev@gmail.com>
2023-09-08 18:48:01 -0700

    Added check for no solution to backward pass. Not tested.

--- puzzle.py
+++ puzzle.py
@@ -31,10 +31,12 @@ class Puzzle:
         self.action_mtx = None
         self.solution = None
         self.solved = False
+        self.generated = False
 
         # Generate state if required
         if not self.state:
             self.state = self.generate_state(dim)
+            self.generated = True  # Important because we know generated puzzles have at least one solution
 
     @staticmethod
     def value_check(state, dim):
@@ -76,18 +78,19 @@ class Puzzle:
 
         return state
 
-    def prettify_state(self):
-        return self.state.toarray().astype(int)
+    @staticmethod
+    def prettify(matrix):
+        return matrix.toarray().astype(int)
 
     def create_action_mtx(self):
 
         # Construct the LHS of the extended matrix (the unravelled action matrix)
         data = np.ones((3, self.no_switches))
         data = np.append(data,
-                         [np.tile([1, 1, 0], self.no_switches // 3 + 1)[:self.no_switches],
+                         [np.tile([1] * (self.dim - 1) + [0], self.no_switches // 3 + 1)[:self.no_switches],
                           np.tile([0, 1, 1], self.no_switches // 3 + 1)[:self.no_switches]],
                          axis=0)
-        offsets = np.array([0, -3, 3, -1, 1])
+        offsets = np.array([0, -self.dim, self.dim, -1, 1])
 
         return dok_array(dia_array((data, offsets),
                                    shape=(self.no_switches, self.no_switches),
@@ -96,14 +99,15 @@ class Puzzle:
     def unravel_state(self, state: Union[ArrayLike, int]):
 
         if isinstance(state, int):
-            state = dok_array(np.array([state for _ in range(self.no_switches)]), dtype=np.int8)
+            state = dok_array(np.array([state for _ in range(self.no_switches)]).reshape(self.no_switches, 1),
+                              dtype=np.int8)
 
         else:
             state = dok_array(np.array(state).reshape(1, self.no_switches), dtype=np.int8)
 
         return state
 
-    def solve_puzzle(self, desired_state: Union[ArrayLike, int], parallelize: bool = False):
+    def solve(self, desired_state: Union[ArrayLike, int], parallelize: bool = False):
 
         # Create action matrix and unravel desired state
         self.action_mtx = self.create_action_mtx()
@@ -116,25 +120,53 @@ class Puzzle:
 
         # Forward elimination
         for k in range(self.no_switches - 1):
-            for i in range(k + 1, self.no_switches):
-                if self.action_mtx[k, k] != 0:
 
-                    for j in range(k + 1, self.action_mtx):
+            if self.action_mtx[k, k] == 0:  # Check if pivot needed
+                piv_on = None
+                r = k + 1
+
+                while (not piv_on) and (r <= (self.no_switches - 1)):
+                    if self.action_mtx[r, k] == 1:
+                        piv_on = r
+
+                    r += 1
+
+                if piv_on:  # Perform pivot of rows k and r, for column k through end (also pivot solution)
+
+                    for c in range(k, self.no_switches):
+                        temp = self.action_mtx[k, c]
+                        self.action_mtx[k, c] = self.action_mtx[piv_on, c]
+                        self.action_mtx[piv_on, c] = temp
+
+                    temp = self.solution[k, 0]
+                    self.solution[k, 0] = self.solution[piv_on, 0]
+                    self.solution[piv_on, 0] = temp
+
+            if self.action_mtx[k, k] != 0:
+
+                for i in range(k + 1, self.no_switches):
+
+                    for j in range(k + 1, self.no_switches):
                         self.action_mtx[i, j] = self.action_mtx[i, j] ^ self.action_mtx[i, k] * self.action_mtx[k, j]
 
-                    self.solution[i] = self.solution[i] ^ self.action_mtx[i, k] * self.solution[k]
+                    self.solution[i, 0] = self.solution[i, 0] ^ self.action_mtx[i, k] * self.solution[k, 0]
                     self.action_mtx[i, k] = 0  # Variable no longer needed. Zero out to save memory.
+
+        print(self.prettify(self.action_mtx))
 
         # Backward substitution
         for i in reversed(range(self.no_switches)):
             for j in range(i, self.no_switches):
-                self.solution[i] = self.solution[i] ^ self.action_mtx[i, j] * self.solution[j]
+                self.solution[i, 0] = self.solution[i, 0] ^ self.action_mtx[i, j] * self.solution[j, 0]
 
             if self.action_mtx[i, i] != 0:
-                self.solution[i] = self.solution[i] / self.action_mtx[i, i]
+                self.solution[i, 0] = self.solution[i, 0] / self.action_mtx[i, i]
 
-            else:  # Zero diagonal implies solving 0x = 0 which has two solutions in Z2: 0 and 1.
-                self.solution[i] = 0
+            elif self.solution[i, 0] == 0:  # Solving 0x = 0 which has two solutions in Z2: 0 and 1.
+                self.solution[i, 0] = 0
+
+            else:
+                raise ValueError("No solution exists.")
 
         # Change solved flag to True
         self.solved = True
@@ -145,5 +177,6 @@ class Puzzle:
 # =================================================================== #
 
 if __name__ == "__main__":
-    my_puzzle = Puzzle(dim=5, seed=SEED)
-    print(my_puzzle.prettify_state())
+    my_puzzle = Puzzle(dim=4, seed=SEED)
+    print(my_puzzle.prettify(my_puzzle.state))
+    my_puzzle.solve(desired_state=1)
