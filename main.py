a4bd7872db75fea07e30641c84c4ca498415745b
Karla Hernandez <karlahrnndz.dev@gmail.com>
2023-09-15 23:13:46 -0700

    Refactor to match terminology in article.

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from numpy.typing import ArrayLike
 from numpy.random import default_rng
 from scipy.sparse import dia_array
 from typing import Union
+from numba import jit
 import numpy as np
 import warnings
 import time
 import json
-
 
 # =================================================================== #
 #                              Constants                              #
@@ -20,49 +20,50 @@ SEED = 42
 # =================================================================== #
 
 class Puzzle:
-    """A representation of the lights-off start_state's start_state."""
+    """A representation of the lights-off init_state's init_state."""
 
     def __init__(self,
-                 start_state: ArrayLike = None,
+                 init_state: ArrayLike = None,
                  gen_state: tuple = None,
                  seed: int = None):
 
-        self.start_state, self.gen_state, self.transposed = self.value_check(start_state, gen_state)
-        if self.start_state is None:
+        self.init_state, self.gen_state, self.transposed = self.value_check(init_state, gen_state)
+        if self.init_state is None:
             self.gen = default_rng(seed=seed)
-            self.start_state = self.generate_state()
+            self.init_state = self.generate_state()
 
-        self.dim = self.start_state.shape
+        self.dim = self.init_state.shape
         self.max_dim = max(self.dim[0], self.dim[1])
         self.no_switches = self.dim[0] * self.dim[1]
-        self.action_mtx = None
+        self.toggle_mtx = None
         self.solution = None
 
     @staticmethod
-    def value_check(start_state, gen_state):
+    def value_check(init_state, gen_state):
         transposed = False
 
-        if start_state is not None:
-            start_state = np.array(start_state, dtype=np.int8)
+        if init_state is not None:
+            init_state = init_state.astype(np.int8) if isinstance(init_state, np.ndarray) \
+                else np.array(init_state, dtype=np.int8)
 
-            if start_state.shape[0] > start_state.shape[1]:
-                start_state = start_state.T
+            if init_state.shape[0] > init_state.shape[1]:
+                init_state = init_state.T
                 transposed = True
 
-            # Warn when gen_state is present as well as staten and force gen_state to match start_state
+            # Warn when gen_state is present as well as staten and force gen_state to match init_state
             if gen_state:
-                warnings.warn(f"Both `start_state` and `gen_state` were provided."
+                warnings.warn(f"Both `init_state` and `gen_state` were provided."
                               f"Setting `gen_state = None.")
                 gen_state = None
 
         elif gen_state is None:
-            raise ValueError("One of `start_state` and `gen_state` must be provided.")
+            raise ValueError("One of `init_state` and `gen_state` must be provided.")
 
         elif gen_state[0] > gen_state[1]:
             gen_state = (gen_state[1], gen_state[0])
             transposed = True
 
-        return start_state, gen_state, transposed
+        return init_state, gen_state, transposed
 
     def generate_state(self):
         state = np.zeros(self.gen_state, dtype=np.int8)
@@ -84,7 +85,27 @@ class Puzzle:
 
         return state
 
-    def create_action_mtx(self):
+    def update_final_state(self, final_state: Union[ArrayLike, int]):
+
+        if isinstance(final_state, int):
+            final_state = np.array([final_state for _ in range(self.no_switches)], dtype=np.int8)
+
+        else:
+            final_state = np.array(final_state, dtype=np.int8).ravel()
+
+        return np.mod(self.init_state.ravel() + final_state, 2)
+
+    def solve(self, final_state: Union[ArrayLike, int]):
+
+        # Create action matrix and unravel desired init_state
+        self.toggle_mtx = self.create_toggle_mtx()
+        self.solution = self.update_final_state(final_state)
+
+        # Implement gaussian elimination (function implemented separately to allow use of numba)
+        self.solution, self.init_state = \
+            gauss_elim(self.no_switches, self.toggle_mtx, self.solution, self.dim, self.transposed, self.init_state)
+
+    def create_toggle_mtx(self):
 
         if self.dim == (1, 1):
             return np.array([[1]], dtype=np.int8)
@@ -95,114 +116,102 @@ class Puzzle:
                 np.tile([1] * (self.max_dim - 1) + [0], (self.max_dim ** 2) // 3 + 1)[:self.max_dim ** 2],
                 np.tile([0] + [1] * (self.max_dim - 1), (self.max_dim ** 2) // 3 + 1)[:self.max_dim ** 2]], axis=0)
             offsets = np.array([0, -self.max_dim, self.max_dim, -1, 1])
-            action_mtx = (dia_array((data, offsets),
+            toggle_mtx = (dia_array((data, offsets),
                                     shape=(self.max_dim ** 2, self.max_dim ** 2),
                                     dtype=np.int8)).toarray()
 
         if self.no_switches != self.max_dim ** 2:  # Need to correct action matrix (always more columns than rows)
             sel_idx = [ele for ele in range(self.no_switches)]
-            action_mtx = action_mtx[sel_idx, :][:, sel_idx]
+            toggle_mtx = toggle_mtx[sel_idx, :][:, sel_idx]
 
-        return action_mtx
+        return toggle_mtx
 
-    def prepare_end_state(self, desired_state: Union[ArrayLike, int]):
 
-        if isinstance(desired_state, int):
-            desired_state = np.array([desired_state for _ in range(self.no_switches)], dtype=np.int8)
+# =================================================================== #
+#                              Functions                              #
+# =================================================================== #
+
+@jit(nopython=True)
+def gauss_elim(no_switches, toggle_mtx, solution, dim, transposed, init_state):
+    row_map = list(range(no_switches))
+
+    # Forward elimination
+    for k in range(no_switches - 1):
+        k_idx = row_map[k]
+
+        if toggle_mtx[k_idx, k] == 0:  # Check if pivot needed
+            piv_on = -1
+            r = k + 1
+
+            while (piv_on < 0) and (r <= (no_switches - 1)):
+                r_idx = row_map[r]
+
+                if toggle_mtx[r_idx, k] == 1:
+                    piv_on = r
+                r += 1
+
+            if piv_on >= 0:  # Perform pivot of rows k and r, for column k through end (also pivot solution)
+                temp = row_map[k]
+                row_map[k] = row_map[piv_on]
+                row_map[piv_on] = temp
+                k_idx = row_map[k]
+
+        if toggle_mtx[k_idx, k] != 0:
+
+            for i in range(k + 1, no_switches):
+                i_idx = row_map[i]
+                coeff = toggle_mtx[i_idx, k]
+
+                if coeff != 0:
+                    toggle_mtx[i_idx, :] = np.mod(toggle_mtx[i_idx, :] + toggle_mtx[k_idx, :], 2)
+                    solution[i_idx] = (solution[i_idx] + solution[k_idx]) % 2
+
+    # Backward substitution
+    for i in range(no_switches - 1, -1, -1):
+        i_idx = row_map[i]
+
+        if i != (no_switches - 1):
+
+            for j in range(i + 1, no_switches):
+                j_idx = row_map[j]
+                solution[i_idx] = (solution[i_idx] + toggle_mtx[i_idx, j] * solution[j_idx]) % 2
+
+        if toggle_mtx[i_idx, i] != 0:
+            solution[i_idx] = (solution[i_idx] / toggle_mtx[i_idx, i]) % 2
+
+        elif solution[i_idx] == 0:  # Solving 0x = 0 which has two solutions in Z2: 0 and 1.
+            solution[i_idx] = 0
 
         else:
-            desired_state = np.array(desired_state, dtype=np.int8).ravel()
+            raise ValueError("No solution exists.")
 
-        return np.mod(self.start_state.ravel() + desired_state, 2)
+    # Format solution
+    solution = np.array([solution[row_map[i]] for i in range(no_switches)]).reshape(dim)
 
-    def solve(self, end_state: Union[ArrayLike, int]):
+    if transposed:
+        solution = solution.T
+        init_state = init_state.T
 
-        # Create action matrix and unravel desired start_state
-        self.action_mtx = self.create_action_mtx()
-        self.solution = self.prepare_end_state(end_state)
-
-        # Implement gaussian elimination
-        self.gauss_elim()
-
-    def gauss_elim(self):
-
-        row_map = {i: i for i in range(self.no_switches)}
-
-        # Forward elimination
-        for k in range(self.no_switches - 1):
-            k_idx = row_map[k]
-
-            if self.action_mtx[k_idx, k] == 0:  # Check if pivot needed
-                piv_on = None
-                r = k + 1
-
-                while (not piv_on) and (r <= (self.no_switches - 1)):
-                    r_idx = row_map[r]
-
-                    if self.action_mtx[r_idx, k] == 1:
-                        piv_on = r
-                    r += 1
-
-                if piv_on:  # Perform pivot of rows k and r, for column k through end (also pivot solution)
-                    temp = row_map[k]
-                    row_map[k] = row_map[piv_on]
-                    row_map[piv_on] = temp
-                    k_idx = row_map[k]
-
-            if self.action_mtx[k_idx, k] != 0:
-
-                for i in range(k + 1, self.no_switches):
-                    i_idx = row_map[i]
-                    coeff = self.action_mtx[i_idx, k]
-
-                    if coeff != 0:
-                        self.action_mtx[i_idx, :] = np.mod(self.action_mtx[i_idx, :] + self.action_mtx[k_idx, :], 2)
-                        self.solution[i_idx] = (self.solution[i_idx] + self.solution[k_idx]) % 2
-
-        # Backward substitution
-        for i in reversed(range(self.no_switches)):
-            i_idx = row_map[i]
-
-            if i != (self.no_switches - 1):
-
-                for j in range(i + 1, self.no_switches):
-                    j_idx = row_map[j]
-                    self.solution[i_idx] = \
-                        (self.solution[i_idx] + self.action_mtx[i_idx, j] * self.solution[j_idx]) % 2
-
-            if self.action_mtx[i_idx, i] != 0:
-                self.solution[i_idx] = (self.solution[i_idx] / self.action_mtx[i_idx, i]) % 2
-
-            elif self.solution[i_idx] == 0:  # Solving 0x = 0 which has two solutions in Z2: 0 and 1.
-                self.solution[i_idx] = 0
-
-            else:
-                raise ValueError("No solution exists.")
-
-        # Format solution
-        self.solution = np.array([self.solution[row_map[i]] for i in range(self.no_switches)]).reshape(self.dim)
-
-        if self.transposed:
-            self.solution = self.solution.T
-            self.start_state = self.start_state.T
-
+    return solution, init_state
 
 # =================================================================== #
 #                              Execution                              #
 # =================================================================== #
 
+
 if __name__ == "__main__":
     times = {}
 
-    for n in range(1, 201):
+    for n in range(1, 101):
         avg = 0
 
         for sims in range(5):
             start = time.time()
-            puzzle = Puzzle(start_state=np.zeros((n, n)), gen_state=None, seed=None)
-            puzzle.solve(end_state=1)
+            puzzle = Puzzle(init_state=np.zeros((n, n)), gen_state=None, seed=None)
+            puzzle.solve(final_state=1)
             delta = time.time() - start
-            times[n] = (avg * sims + delta) / (sims + 1)
+            avg = (avg * sims + delta) / (sims + 1)
+        times[n] = avg
 
         with open('times.json', 'w') as file:
             json.dump(times, file)
